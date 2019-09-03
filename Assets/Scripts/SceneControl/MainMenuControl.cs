3c6633771ef292e665e35528a1df9d0650cbfd1b
Awesome-Rex <john.esqqq@gmail.com>
2019-09-02 21:51:49 -0400

    working on main menu animation

---
+++ Assets/Scripts/SceneControl/MainMenuControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuControl : MonoBehaviour
+{
+    public enum MainMenuState
+    {
+        Title, Selection
+    }
+
+    public MainMenuState mainMenuState;
+
+    public bool canTransition;
+
+    public void enableTransitions ()
+    {
+        canTransition = true;
+    }
+
+    public void disableTransitions ()
+    {
+        canTransition = false;
+    }
+
+    private Animator animator;
+
+    public void backToTitle ()
+    {
+        if (mainMenuState == MainMenuState.Selection && canTransition)
+        {
+            mainMenuState = MainMenuState.Title;
+            animator.SetTrigger("Switch");
+        }
+    }
+
+    private void Awake()
+    {
+        mainMenuState = MainMenuState.Title;
+
+        canTransition = false;
+
+        animator = /*transform.parent.*/GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mainMenuState == MainMenuState.Title && (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)) && canTransition)
+        {
+            mainMenuState = MainMenuState.Selection;
+            animator.SetTrigger("Switch");
+        }
+    }
+}
